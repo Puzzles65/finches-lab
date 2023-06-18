75359ce01b06bcabcf8bb24aa264ffc48448bf83
Puzzles65 <chitrungnguyen12@gmail.com>
2023-06-18 19:24:47 +0100

    addingTemplate

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Finch 
 
 
@@ -15,6 +16,10 @@ def finches_index(request):
         'finches': finches
     })
 
-def finches_details(request, finch_id):
+def finches_detail(request, finch_id):
     finch = Finch.objects.get(id = finch_id)
-    return render(request, 'finches/details.html', {'finch': finch})
+    return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
